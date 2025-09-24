a21d33148b5a0a8dd13186eecd89906618237d97
Rehan111-555 <rehan.saleh@caresynchealth.com>
2025-09-24 20:05:07 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,15 +1,8 @@
-// middleware.js
 export { default } from "next-auth/middleware";
 
-/**
- * Protect everything except:
- * - /signin
- * - Next internals: /api/auth, /_next/static, /_next/image
- * - Common root files: /favicon.ico, /robots.txt, /sitemap.xml
- * - Any request that looks like a static file (has a dot in the last segment)
- */
 export const config = {
   matcher: [
+    // Protect everything except the following:
     "/((?!signin|api/auth|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)",
   ],
 };
