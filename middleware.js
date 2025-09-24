0552b376fb8d55ce27bbf027db83b5c01b832a8d
Rehan111-555 <rehan.saleh@caresynchealth.com>
2025-09-24 20:13:16 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    // Protect everything except the following:
+    // Protect everything except the login and assets
     "/((?!signin|api/auth|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)",
   ],
 };
