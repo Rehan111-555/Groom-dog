b5f93b63a5db48ad75fd47fd187395a10f0c5615
Rehan111-555 <rehan.saleh@caresynchealth.com>
2025-11-13 21:40:50 +0500

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,6 +1,6 @@
 // app/api/register/route.js
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
